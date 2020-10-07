954413b42eecf9db4496cf3ecdefaa672e2195ef
Mary Baker <meb601@gmail.com>
2020-10-07 13:13:41 -0400

    Write server.get in server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const dbConfig = require("../data/db-config.js");
 
 const UserRouter = require("../users/user-router.js");
 
@@ -9,5 +10,17 @@ server.use(helmet());
 server.use(express.json());
 
 server.use("/api/users", UserRouter);
+
+server.get("/api/posts", (req, res) => {
+  dbConfig("posts as p")
+    .join("users as u", "p.user_id", "=", "u.id")
+    .select("p.id", "p.contents", "u.username as postedBy")
+    .then((posts) => {
+      res.status(200).json({ data: posts });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
 
 module.exports = server;
